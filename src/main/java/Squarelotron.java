c244a9f42f076fa543f438dc2fc1f62d103e081d
Mher <poghosyanusc@gmail.com>
2017-11-12 17:54:38 -0800

    Initial commit

---
+++ src/main/java/Squarelotron.java
@@ -0,0 +1,20 @@
+public class Squarelotron {
+    private int[][] squarelotron;
+    private int size;
+
+    public Squarelotron(int n) {
+
+    }
+
+    public Squarelotron upsideDownFlip(int ring) {
+        return new Squarelotron(0);
+    }
+
+    public Squarelotron mainDiagonalFlip(int ring) {
+        return new Squarelotron(0);
+    }
+
+    public void rotateRight(int numberOfTurns) {
+
+    }
+}
---
+++ src/test/java/SquarelotronTest.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mher on 11/12/2017.
+ */
+public class SquarelotronTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void upsideDownFlip() throws Exception {
+    }
+
+    @Test
+    public void mainDiagonalFlip() throws Exception {
+    }
+
+    @Test
+    public void rotateRight() throws Exception {
+    }
+
+}
