feb9b04859078797062b53078713a79fa7bde4d6
Mher <poghosyanusc@gmail.com>
2017-11-13 08:40:43 -0800

    set up to do at work

--- src/main/java/Squarelotron.java
+++ src/main/java/Squarelotron.java
@@ -1,9 +1,16 @@
 public class Squarelotron {
     private int[][] squarelotron;
+
     private int size;
-
     public Squarelotron(int n) {
+        squarelotron = new int[n][n];
+        size = n;
 
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                squarelotron[i][j] = i*n + j + 1;
+            }
+        }
     }
 
     public Squarelotron upsideDownFlip(int ring) {
@@ -17,4 +24,14 @@ public class Squarelotron {
     public void rotateRight(int numberOfTurns) {
 
     }
+
+    public int[][] getSquarelotron() {
+        return squarelotron;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+
 }
--- src/test/java/SquarelotronTest.java
+++ src/test/java/SquarelotronTest.java
@@ -7,12 +7,21 @@ import static org.junit.Assert.*;
  * Created by Mher on 11/12/2017.
  */
 public class SquarelotronTest {
+
+    private Squarelotron squarelotron;
     @Before
     public void setUp() throws Exception {
+        squarelotron = new Squarelotron(3);
     }
 
     @Test
     public void upsideDownFlip() throws Exception {
+        for (int i = 0; i < squarelotron.getSize(); ++i) {
+            for (int j = 0; j < squarelotron.getSize(); ++j) {
+                assertEquals((squarelotron.getSize() - i - 1) * squarelotron.getSize() + j + 1,
+                        squarelotron.getSquarelotron()[i][j]);
+            }
+        }
     }
 
     @Test
