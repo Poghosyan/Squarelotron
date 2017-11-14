1c578330e872fe7dfb65d9d96d1412b54cdf3fec
mpoghosyan <mher.poghosyan@loni.usc.edu>
2017-11-13 16:49:40 -0800

    Added upsideDownFlip testing code

--- src/test/java/SquarelotronTest.java
+++ src/test/java/SquarelotronTest.java
@@ -3,33 +3,44 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by Mher on 11/12/2017.
- */
 public class SquarelotronTest {
 
-    private Squarelotron squarelotron;
-    @Before
-    public void setUp() throws Exception {
-        squarelotron = new Squarelotron(3);
-    }
+    private final int MAX_SQUARELOTRON_DIMENSION = 8;
+
 
     @Test
     public void upsideDownFlip() throws Exception {
-        for (int i = 0; i < squarelotron.getSize(); ++i) {
-            for (int j = 0; j < squarelotron.getSize(); ++j) {
-                assertEquals((squarelotron.getSize() - i - 1) * squarelotron.getSize() + j + 1,
-                        squarelotron.getSquarelotron()[i][j]);
+        for (int i = 1; i < MAX_SQUARELOTRON_DIMENSION; ++i) {
+            Squarelotron squarelotron = new Squarelotron(i);
+            int size = squarelotron.getSize();
+            int ringLimit = (int)Math.ceil((double)squarelotron.getSize()/2.0);
+            for (int ring = 1; ring < ringLimit; ++ring) {
+                squarelotron.upsideDownFlip(ring);
+                checkUpsideDownFlip(ringLimit, squarelotron, size);
             }
         }
     }
 
     @Test
     public void mainDiagonalFlip() throws Exception {
+
     }
 
     @Test
     public void rotateRight() throws Exception {
+    }
+
+    private void checkUpsideDownFlip(int ringLimit, Squarelotron squarelotron, int size) {
+        for (int ringNumber = 1; ringNumber < ringLimit; ++ringNumber) {
+            squarelotron.upsideDownFlip(ringNumber);
+            for (int i = 0; i < size ; ++i) {
+                for (int j = 0; j < size; ++j) {
+                    if ((j >= ringNumber - 1 || j <= size - ringNumber) && (i >= ringNumber || i <= size - ringNumber)) {
+                        assertEquals((size - i - 1) * size + j + 1, squarelotron.getSquarelotron()[i][j]);
+                    }
+                }
+            }
+        }
     }
 
 }
